87bbff27170a6db1385ecbdcc0e4ca388aa8e5cd
Tobi Onipede <onipede89@gmail.com>
2023-11-04 14:22:28 +0100

    implement simple HTTP auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: 'jungle', password: 'book' do |username, password|
+  username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
+end
+
+
   def show
   end
 end
